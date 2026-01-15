c9f55a3e5c6a50f897ce05038dd1562eaf0d36f9
RAjzg <mdnurnobihaque79@gmail.com>
2026-01-15 08:37:27 +0600

    Update gojol.js

--- Script/commands/gojol.js
+++ Script/commands/gojol.js
@@ -25,7 +25,7 @@ module.exports.run = async ({
 }) => {
  const _0x46768b = global.nodemodule.request;
  const _0x23c96a = global.nodemodule["fs-extra"];
- var _0x40023d = [" 🌸ইসলামিক || গজল🍒🐰\nuse headphone for batter sound \nCredit: Shaon Ahmed🐰"];
+ var _0x40023d = [" 🌸ইসলামিক || গজল🍒🐰\𝐍𝐔𝐒𝐄 𝐇𝐄𝐀𝐃𝐏𝐇𝐎𝐍𝐄 𝐅𝐎𝐑 𝐁𝐀𝐓𝐓𝐄𝐑 𝐒𝐎𝐔𝐍𝐃 \n𝐂𝐑𝐄𝐀𝐓𝐎𝐑:➪ 𝐌𝐃:𝐍𝐔𝐑𝐍𝐎𝐁𝐈 𝐇𝐀𝐐𝐔𝐄"];
  var _0xa3ec84 = _0x40023d[Math.floor(Math.random() * _0x40023d.length)];
  var _0x3fdfb0 = ["https://drive.google.com/uc?id=1xjyq3BrlW3bGrp8y7eedQSuddCbdvLMN", "https://drive.google.com/uc?id=1ySwrEG6xVqPdY5BcBP8I3YFCUOX4jV9e", "https://drive.google.com/uc?id=1xnht0PdBt9DnLGzW7GmJUTsTIJnxxByo", "https://drive.google.com/uc?id=1yHB48N_wPJnU5uV18KMZOLNqo5NE7L4W", "https://drive.google.com/uc?id=1xpwuubDL_ebjKJhujb-Ee-FikUF92oF6", "https://drive.google.com/uc?id=1yK0A3lyIJoPRp6g3UjNrC31n0yLfc1Ht", "https://drive.google.com/uc?id=1xrwhHLhsdKVAn_9umLfUysCt0S2v5QWe", "https://drive.google.com/uc?id=1yKwewV-oYbn57lGnlACykSD-yt8fOsfT", "https://drive.google.com/uc?id=1xulSi_qyJA47sF9rC9BUIPyBqh47t9Ls", "https://drive.google.com/uc?id=1y-PIYYziv-m8QRwmMBWCTl2wzuH8NpYJ", "https://drive.google.com/uc?id=1y0wV96m-notKVHnuNdF8xVCWiockSiME", "https://drive.google.com/uc?id=1xxMQnp-9-4BoLrGpReps93JQv4k8WUOP"];
  var _0x5ed4db = () => _0x49e1e3.sendMessage({
